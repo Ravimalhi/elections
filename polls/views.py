645ffdcbcbef6c8a3a3ad83018839c0286655828
Ravi <mravipartap@gmail.com>
2019-09-19 19:11:36 -0400

    few new voting constraints
    new features included

--- polls/views.py
+++ polls/views.py
@@ -2,29 +2,26 @@ from django.shortcuts import render, HttpResponse
 from .models import vote, voter
 # Create your views here.
 def Polls(request):
-    if request.method == "POST":
-        vid = request.POST.get('ID')
-        result = voter.objects.get(voterId=vid)
-        if result:
-            if result.casted == 0:
-                result.casted +=1
-                result.save()
-                return render(request, "polls/vote.html")
-            else:
-                return HttpResponse("You already casted vote")
-        else:
-            return HttpResponse("improper adhar card")
-    return render(request, "polls/home.html")
+   return render(request, "polls/home.html")
 
 def Cast(request):
     if request.method == "POST":
-        cast = request.POST.get('votecas')
+        vid = request.POST.get('ID')
         can1 = 'liberals'
         can2 = 'conservative'
         can3 = 'ndp'
         can4 = 'green'
         can5 =  'bloc'
         result = vote.objects.get(id=1)
+        result2 = voter.objects.get(voterId=vid)
+        if result2.casted == 1:
+            return HttpResponse("<h1>you already casted the vote</h1>")
+        #Checking if vote is casted already.....
+        if result2.casted == 0:
+            result2.casted +=1
+            result2.save()
+
+        cast = request.POST.get('votecas')
         if cast == can1:
             result.liberals = result.liberals+1
             result.save()
